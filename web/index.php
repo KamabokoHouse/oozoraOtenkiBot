a1c8cb2825cd1ff4da1ef864760d34d433dd6e28
shoma <nerorubymoon@gmail.com>
2018-07-29 23:59:27 +0900

    sa

--- web/index.php
+++ web/index.php
@@ -1,4 +1,5 @@
 <?php
+date_default_timezone_set('Asia/Tokyo');
 
 //チャンネルアクセストークン
 $channelAccessToken = getenv('channelAccessToken');
@@ -49,7 +50,7 @@ switch ($weather) {
 
 // TODO: 返答用の文字列形成
 	$date = new DateTime('now');
-	$NowDateTime = $date->format('G時i分');
+	$NowDateTime = $date->format('H時i分');
 
 	$messageText = "時刻は${NowDateTime}!
 今日のお空はどんな空〜?
