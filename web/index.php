fbe98d396e372ffbb313d542743de68c36cb783e
shoma <nerorubymoon@gmail.com>
2018-07-31 00:31:42 +0900

    a

--- web/index.php
+++ web/index.php
@@ -15,7 +15,7 @@ $eventType = $jsonObj->{"events"}[0]->{"type"};
 
 $weatherToken = getenv('weatherApiToken');
 
-// TODO:お天気取得API
+//OpenWhetherMapからお天気情報を取得します
 $weather = "";
 $urlContents = file_get_contents("http://api.openweathermap.org/data/2.5/weather?q=Tokyo&appid=".$weatherToken);
 $weatherArray = json_decode($urlContents, true);    //連想配列の場合は第2引数へtrueを指定
@@ -25,35 +25,40 @@ $tempMax = $weatherArray['main']['temp_max'] - 273.15;
 $tempMin = $weatherArray['main']['temp_min'] - 273.15;
 
 // 0xを抜いた数字の部分
-$code = '1000AC';
-// 16進エンコードされたバイナリ文字列をデコード
-$bin = hex2bin(str_repeat('0', 8 - strlen($code)) . $code);
-// UTF8へエンコード
-$emoticon =  mb_convert_encoding($bin, 'UTF-8', 'UTF-32BE');
 
 switch ($weather) {
 	case 'Thunderstorm':
+		$code = '10003A';
 		$weatherToJp = "雷";
 		break;
 	case 'Drizzle':
 		$weatherToJp = "小雨";
 		break;
 	case 'Rain':
+		$code = '1000AA';
 		$weatherToJp = '雨';
 		break;
 	case 'Snow':
+		$code = '1000AB';
 		$weatherToJp = '雪';
 		break;
 	case 'Clear':
+		$code = '1000A9';
 		$weatherToJp = '晴れ';
 		break;
 	case 'Clouds':
-		$weatherToJp = "曇り" . $emoticon;
+		$code = '1000AC';
+		$weatherToJp = "曇り";
 		break;
 	default:
 	  $weatherToJp = '分からないです';
 		break;
 }
+
+// 16進エンコードされたバイナリ文字列をデコード
+$bin = hex2bin(str_repeat('0', 8 - strlen($code)) . $code);
+// UTF8へエンコード
+$emoticon =  mb_convert_encoding($bin, 'UTF-8', 'UTF-32BE');
 
 // TODO: 返答用の文字列形成
 	$date = new DateTime('now');
@@ -63,14 +68,34 @@ switch ($weather) {
 今日のお空はどんな空〜?
 大空お天気の時間です!
 
-今日の都心部は${weatherToJp}!
+今日の都心部は${weatherToJp}${emoticon}!
 最高気温は${tempMax}度、最低気温は${tempMin}度です!
 
 それでは通勤通学気をつけて、行ってらっしゃ〜い";
 
-//メッセージイベントだった場合です
-//テキスト、画像、スタンプなどの場合「message」になります
-//他に、follow postback beacon などがあります
+//友達追加時
+if($eventType == 'follow'){
+	$followMessage = "友達になってくれてありがとう！
+こちらは大空お天気です！
+
+「お天気」と呼びかけてくれると今のお天気をお知らせします！"
+
+//ReplyToken取得
+//受信したイベントに対して返信を行うために必要になります
+$replyToken = $jsonObj->{"events"}[0]->{"replyToken"};
+
+ $response_format_text = [
+	 "type" => "text",
+	 "text" => $followMessage
+ ];
+
+ $post_data = [
+	 "replyToken" => $replyToken,
+	 "messages" => [$response_format_text]
+ ];
+}
+
+//メッセージイベントだった場合
 $validMessage = $jsonObj->{"events"}[0]->{"message"}->{"text"};
 
 if ($eventType == 'message') {
